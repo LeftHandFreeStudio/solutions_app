af6db5043a81da94e3154785c30d892ee9be91ef
Maciej Kawka <maciej.kawka@objective-partner.com>
2019-03-28 18:20:51 +0100

    implemented steps in solutions

--- src/my-solutions-list/solution/Solution.js
+++ src/my-solutions-list/solution/Solution.js
@@ -4,15 +4,22 @@ import './Solution.css';
 class Solution extends Component {
   render() {
     const solution = this.props.solution;
+    const steps = solution.steps ? solution.steps : [];
     const index = this.props.index + 1;
+    const renderedSteps = steps.map((step, i) => (
+      <div key={i}>
+        <p>Step {' ' + index}</p>
+        <p className="step">{step}</p>
+      </div>
+    ));
     return (
       <div>
         <p>{index}</p>
         <p className="tags-info">{solution.tags}</p>
+        <div>{renderedSteps}</div>
         <p className="description">{solution.description}</p>
       </div>
     );
   }
 }
-
 export default Solution;
