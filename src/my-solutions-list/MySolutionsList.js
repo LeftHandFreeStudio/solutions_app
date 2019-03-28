7bd5dc6f4e4bf2b5bf9a5262464fdae640a74e68
Maciej Kawka <maciej.kawka@objective-partner.com>
2019-03-28 15:32:14 +0100

    added test to MySolutionsList comp

--- src/my-solutions-list/MySolutionsList.js
+++ src/my-solutions-list/MySolutionsList.js
@@ -4,14 +4,16 @@ import Solution from './solution/Solution';
 
 class MySolutionsList extends Component {
   render() {
-    const solutionsList = this.props.solutionsList;
+    const solutionsList = this.props.solutionsList
+      ? this.props.solutionsList
+      : [];
     const userInfo = solutionsList[0]
       ? 'Solutions from user : ' + solutionsList[0].user
       : '';
     return (
       <div>
-        <p>{userInfo}</p>
-        {this.props.solutionsList.map((solution, index) => {
+        <p className="user-info">{userInfo}</p>
+        {solutionsList.map((solution, index) => {
           return (
             <Solution key={solution.id} index={index} solution={solution} />
           );
