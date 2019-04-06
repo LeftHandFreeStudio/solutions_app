ecceee1710015dbfd3ebd622de8bf865377d11bf
Maciej Kawka <maciej.kawka@objective-partner.com>
2019-04-06 09:03:34 +0200

    added sync actions for fetching solutions

---
+++ src/modules/actionTypes.js
@@ -0,0 +1,3 @@
+export const FETCH_SOLUTIONS = 'main_solutions_fetch';
+export const RECEIVE_SOLUTIONS = 'main_solutions_fetch';
+export const ADD_SOLUTION = 'add_solution';
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux';
-import * as reducers from './modules/reducers';
+import * as reducers from './modules/solutionsList';
 
 const solutionsApp = combineReducers(reducers);
 export const store = createStore(solutionsApp);
