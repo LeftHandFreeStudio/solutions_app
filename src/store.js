0623d6dd56a1fe074079e8032f96a217a30a1f48
Maciej Kawka <maciej.kawka@objective-partner.com>
2019-04-06 08:30:55 +0200

    added fractal project structure

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux';
-import * as reducers from './reducers';
+import * as reducers from './modules/reducers';
 
 const solutionsApp = combineReducers(reducers);
 export const store = createStore(solutionsApp);
